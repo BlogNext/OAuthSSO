d9ec5b0d988e696d789c07a6f58f4ace0b076656
qixi <qixi@beihai365.com>
2021-03-25 11:14:54 +0800

    创建预授权码

--- service/oauth/model/user.go
+++ service/oauth/model/user.go
@@ -1,9 +1,9 @@
 package model
 
-
 type UserModel struct {
 	BaseModel
 	Nickname string `gorm:"cloumn:nickname"`
+	Password string `gorm:"cloumn:password"`
 }
 
 func (UserModel) TableName() string {
--- service/oauth/service/auth.go
+++ service/oauth/service/auth.go
@@ -1,12 +1,14 @@
 package service
 
 import (
+	"crypto/sha1"
 	"fmt"
 	"github.com/OauthSSO/service/common/cache/lru"
 	"github.com/OauthSSO/service/common/db"
 	"github.com/OauthSSO/service/exception"
 	"github.com/OauthSSO/service/oauth/entity"
 	"github.com/OauthSSO/service/oauth/model"
+	"strings"
 	"time"
 )
 
@@ -40,6 +42,14 @@ func (a *auth) CreatePreAuthCode(request *entity.CreatePreAuthCodeRequest) (resp
 	if err := mysqlDB.Where("nickname = ?", request.Nickname).First(userModel).Error; err != nil {
 		//没有数据
 		panic(exception.NewException(exception.ParamErr, err.Error()))
+	}
+
+	//加密
+	h := sha1.New()
+	h.Write([]byte(request.Password))
+	inputPassword := fmt.Sprintf("%x", h.Sum(nil))
+	if strings.Compare(inputPassword, userModel.Password) != 0 {
+		panic(exception.NewException(exception.ParamErr, "密码错误"))
 	}
 
 	//预授权码的格式： preAuthCode:clientId:userId
