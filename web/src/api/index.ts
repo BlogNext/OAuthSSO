e8e329f4bf00873551b2d561aec2efcffd759099
LaughingZhu <474268433@qq.com>
2021-05-19 14:41:54 +0800

    login 域名修改

--- web/src/api/index.ts
+++ web/src/api/index.ts
@@ -3,14 +3,14 @@
  * @Author: LaughingZhu
  * @Date: 2021-04-27 16:45:02
  * @LastEditros: 
- * @LastEditTime: 2021-05-14 17:20:08
+ * @LastEditTime: 2021-05-19 14:41:41
  */
 
 import request from "@/utils/request"
 
 // 获取pre_auth_code
 export const create = async(data: any) => {
-  return request(`https://log.laughingzhu.cn/api/oauth/create_pre_auth_code`, {
+  return request(`https://login.laughingzhu.cn/api/oauth/create_pre_auth_code`, {
     method: 'POST',
     data
   })
